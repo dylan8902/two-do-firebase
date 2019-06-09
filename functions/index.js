1372114aa5d98f4aeb8aef9cba35ba489e1c0445
Dylan Jones <dyl@anjon.es>
2019-06-09 23:44:44 +0100

    Add hosting and firestore configuration

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.reminders = functions.pubsub
+  .topic('reminders')
+  .onPublish((message) => {
+    console.log('A message on the reminders topic');
+    if (message.json) {
+      console.log('Message Data: ', message.json);
+    }
+    return true;
+  });
+
